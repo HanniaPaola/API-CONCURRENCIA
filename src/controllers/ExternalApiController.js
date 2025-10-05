f814f6d8c13f1520210da7485a7edcf4a7a01ac2
HanniaPaola <233273@ids.upchiapas.edu.mx>
2025-10-04 19:48:54 -0600

    Implementacion

--- src/controllers/ExternalApiController.js
+++ src/controllers/ExternalApiController.js
@@ -1,27 +1,44 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
-class ExternalApiController {
-  static async aggregateApis(req, res) {
-    try {
-      // URLs de las 3 APIs externas
-      const apis = [
+class AggregateService {
+    constructor() {
+        if (AggregateService.instance) return AggregateService.instance;
+        AggregateService.instance = this;
+    }
+
+    async fetchApis() {
+        const urls = [
         'https://api.agify.io?name=michael',
         'https://api.genderize.io?name=michael',
-        'https://api.nationalize.io?name=michael'
-      ];
+        'https://api.nationalize.io?name=michael',
+        'https://www.boredapi.com/api/activity',
+        'https://dog.ceo/api/breeds/image/random',
+        'https://catfact.ninja/fact',
+        'https://api.adviceslip.com/advice',
+        'https://v2.jokeapi.dev/joke/Any',
+        'https://api.coindesk.com/v1/bpi/currentprice.json',
+        'http://universities.hipolabs.com/search?country=Mexico'
+        ];
 
-      // Consumir todas en paralelo
-      const results = await Promise.all(
-        apis.map(url => fetch(url).then(r => r.json()))
-      );
+        const start = Date.now();
 
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ success: true, data: results }));
-    } catch (error) {
-      res.writeHead(500, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ success: false, error: error.message }));
+        try {
+        const responses = await Promise.all(
+            urls.map(url => fetch(url).then(res => res.json()))
+        );
+
+        const duration = Date.now() - start;
+
+        return {
+            success: true,
+            duration: `${duration} ms`,
+            count: responses.length,
+            data: responses
+        };
+        } catch (error) {
+        return { success: false, error: error.message };
+        }
     }
-  }
-}
+    }
 
-module.exports = ExternalApiController;
+    export default new AggregateService();
